242485ed824ec2af6b2152da366974cbc33e2295
kevin <kevinzy17@gmail.com>
2022-09-13 00:37:35 -0700

    implemented: user agent(devices + browser columns), docstrings for each function, installed python user agents package

--- log-parser.py
+++ log-parser.py
@@ -7,13 +7,89 @@
 # 3. translate the useragent to device type (mobile, tablet, desktop) and browser (safari, chrome, etc)
 # 4. combine new geo field (country/state) and device field with existing fields on access log file and output/export as a CSV
 
+from user_agents import parse
 from collections import defaultdict
+
 import re
 import requests
 import csv
 
 
-def reader(filename):
+def extract_useragent(s):
+    """Parses the user agent string into a list of strings
+
+    Args:
+        s (str): the string to be parsed
+
+    Returns:
+        list: A list of strings
+    """
+
+    row = []
+    qe = qp = None  # quote end character (qe) and quote parts (qp)
+    for s in s.replace("\r", "").replace("\n", "").split(" "):
+        if qp:
+            qp.append(s)
+        elif "" == s:  # blanks
+            row.append("")
+        elif '"' == s[0]:  # begin " quote "
+            qp = [s]
+            qe = '"'
+        elif "[" == s[0]:  # begin [ quote ]
+            qp = [s]
+            qe = "]"
+        else:
+            row.append(s)
+
+        l = len(s)
+        if l and qe == s[-1]:  # end quote
+            if l == 1 or s[-2] != "\\":  # don't end on escaped quotes
+                row.append(" ".join(qp)[1:-1].replace("\\" + qe, qe))
+                qp = qe = None
+    return row
+
+
+def get_useragent_info(ua_str):
+    """The get_useragent_info function takes a user agent string and returns a tuple of browser, device type, and operating system
+
+    Args:
+        ua_str (str): The user agent string to parse
+
+    Returns:
+        tuple: A tuple of the browser, device type, and operating system
+    """
+
+    user_agent = parse(ua_str)
+    browser = user_agent.browser.family
+    os = '{}'.format(user_agent.os.family)
+
+    device_type = ""
+
+    if user_agent.is_mobile:
+        device_type = "Mobile"
+    if user_agent.is_tablet:
+        device_type = "Tablet"
+    if user_agent.is_pc:
+        device_type = "Desktop"
+    if user_agent.is_bot:
+        device_type = "Robot"
+
+    device_type = '{}'.format(device_type)
+
+    return browser, device_type, os
+
+
+
+def get_ips(filename):
+    """Read the log file and extract all the IP addresses
+
+    Args:
+        filename (file): the name of the file that contains the log
+
+    Returns:
+        list: A list of strings
+    """
+
     with open(filename) as f:
         log = f.read()
 
@@ -23,11 +99,34 @@ def reader(filename):
         return ip_address_list
 
 
-def find_country_state(ips_list):
+def find_user_info(ips_list, filename):
+    """Takes in a list of IP addresses and a file (expects a log file).
+    It then iterates through the list of IP addresses and uses the user_agent_info list to find the device and browser used.
+    Then it uses the ips_list list to find the country name and state of the IP address.
+
+    Args:
+        ips_list (list): the list of IP addresses we want to track
+        filename (file): the name of the file that contains the logs
+
+    Returns:
+        dictionary: A dictionary with the key being the index of the IP address in the list of IP addresses.
+        The value is a list of tuples. Each tuple contains the IP address, country name, state, and user agent info (device and browser)
+    """
+
+    user_agent_info = []
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            s = line
+            result = extract_useragent(s)
+            ua_string = result[8]
+            user_agent = get_useragent_info(ua_string)
+            user_agent_info.append(user_agent)
+
     output = defaultdict(list)
     temp_list = []
 
-    for ip in ips_list:
+    for index, ip in enumerate(ips_list):
         response = requests.get(f"https://geolocation-db.com/json/{ip}").json()
         country_name = response["country_name"]
         state = response["state"]
@@ -35,8 +134,7 @@ def find_country_state(ips_list):
         if state == None:
             state = "Not Found"
 
-        temp_list.append( (ip, country_name, state) )
-
+        temp_list.append( (ip, country_name, state, user_agent_info[index - 1]) )
 
     for index, info in enumerate(temp_list):
         output[index].append(info)
@@ -45,25 +143,32 @@ def find_country_state(ips_list):
     return output
 
 
-def useragent_converter(useragent):
-    pass
-
 def export_to_csv(dict):
+    """Takes a dictionary and their associated information and exports it to a CSV file
+
+    Args:
+        dict (dictionary): the dictionary to be exported to csv
+    """
+
     with open('output.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
 
-        header = ["IP", "Country Name", "State"]
+        headers = ["IP", "Country Name", "State", "Browser", "Device"]
 
-        writer.writerow(header)
+        writer.writerow(headers)
+
 
         for item in dict.items():
             ip = item[1][0][0]
             country_name = item[1][0][1]
             state = item[1][0][2]
+            user_agent_browser = item[1][0][3][0]
+            user_agent_device = item[1][0][3][1]
 
-            writer.writerow( (ip, country_name, state) )
+
+            writer.writerow( (ip, country_name, state, user_agent_browser, user_agent_device) )
 
 
-# run file
+# runs file
 if __name__ == "__main__":
-    export_to_csv(find_country_state(reader("log2.log")))
+    export_to_csv(find_user_info(get_ips("log2.log"), "log2.log"))
